ae9af142fd84770c977d9a1bb5dc305b61b1ffa7
roslynpius <roslyn.pius@gmail.com>
2023-11-27 21:20:04 +0530

    Initial commit

---
+++ src/StackAndQueue.java
@@ -0,0 +1,48 @@
+import java.util.LinkedList;
+
+// Generic class for implementing Stack using a LinkedList
+class Stack<T> {
+    private LinkedList<T> list;
+
+    // Constructor to initialize the LinkedList
+    public Stack() {
+        list = new LinkedList<>();
+    }
+
+    /**
+     *
+     * @param data The element to be pushed onto the stack.
+     * @desc Pushes an element onto the stack.
+     */
+    public void push(T data) {
+        // Adding the element to the front of the LinkedList
+        list.add(data);
+    }
+
+    /**
+     * @desc Prints the elements of the stack.
+     */
+    public void printStack() {
+        System.out.print("Stack: ");
+        for (T data : list) {
+            System.out.print(data + " ");
+        }
+        System.out.println();
+    }
+}
+
+public class StackAndQueue {
+    public static void main(String[] args) {
+        // Creating a stack of integers
+        Stack<Integer> integerStack = new Stack<>();
+
+        // Pushing elements onto the stack
+        integerStack.push(56);
+        integerStack.push(30);
+        integerStack.push(70);
+
+        // Printing the initial stack
+        System.out.println("Initial Stack:");
+        integerStack.printStack();
+    }
+}
