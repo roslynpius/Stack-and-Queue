127a0c3a56de98f6376751d9614dce0cae98b722
roslynpius <roslyn.pius@gmail.com>
2023-11-27 21:24:08 +0530

    Implemented USECASE 1: Create Stack and Add Elements

--- src/StackAndQueue.java
+++ src/StackAndQueue.java
@@ -10,16 +10,56 @@ class Stack<T> {
     }
 
     /**
+     * Pushes an element onto the stack.
      *
      * @param data The element to be pushed onto the stack.
      * @desc Pushes an element onto the stack.
      */
     public void push(T data) {
         // Adding the element to the front of the LinkedList
-        list.add(data);
+        list.addFirst(data);
     }
 
     /**
+     * Peeks at the element on the top of the stack.
+     *
+     * @return The element at the top of the stack.
+     * @throws IllegalStateException if the stack is empty.
+     * @desc Peeks at the element on the top of the stack.
+     */
+    public T peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return list.getFirst();
+    }
+
+    /**
+     * Pops an element from the stack.
+     *
+     * @return The element popped from the stack.
+     * @throws IllegalStateException if the stack is empty.
+     * @desc Pops an element from the stack.
+     */
+    public T pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return list.removeFirst();
+    }
+
+    /**
+     * Checks if the stack is empty.
+     *
+     * @return true if the stack is empty, false otherwise.
+     * @desc Checks if the stack is empty.
+     */
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    /**
+     * Prints the elements of the stack.
      * @desc Prints the elements of the stack.
      */
     public void printStack() {
@@ -44,5 +84,12 @@ public class StackAndQueue {
         // Printing the initial stack
         System.out.println("Initial Stack:");
         integerStack.printStack();
+
+        // Peeking and popping elements from the stack until it is empty
+        System.out.println("Peek and Pop until the stack is empty:");
+        while (!integerStack.isEmpty()) {
+            System.out.println("Peek: " + integerStack.peek());
+            System.out.println("Pop: " + integerStack.pop());
+        }
     }
 }
